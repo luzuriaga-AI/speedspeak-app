17e6bfe6d40d4550258552ae94890d55a1a56e8c
UNAI LUZURIAGA <unailuzuriaga@MBP-de-UNAI.station>
2025-04-15 02:42:42 +0200

    eliminada ruta /afiliado para AffiliateLanding

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import AuthPage from './pages/AuthPage';
 import BuyCourse from './pages/BuyCourse';
@@ -14,21 +14,19 @@ import AffiliateLanding from './pages/AffiliateLanding'; // ✅ Importar Affilia
 
 const App = () => {
   return (
-    <Router> {/* ✅ Necesario para que funcionen bien las rutas */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/auth" element={<AuthPage />} />
-        <Route path="/buy" element={<BuyCourse />} />
-        <Route path="/dashboard" element={<UserDashboard />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/lesson/:id" element={<Lesson />} />
-        <Route path="/achievements" element={<AchievementsPage />} />
-        <Route path="/videos" element={<VideosPage />} />
-        <Route path="/exams" element={<ExamsPage />} />
-        <Route path="/affiliate" element={<AffiliatePage />} />
-        <Route path="/afiliado" element={<AffiliateLanding />} /> {/* ✅ Aquí está la ruta del link */}
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/auth" element={<AuthPage />} />
+      <Route path="/buy" element={<BuyCourse />} />
+      <Route path="/dashboard" element={<UserDashboard />} />
+      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/lesson/:id" element={<Lesson />} />
+      <Route path="/achievements" element={<AchievementsPage />} />
+      <Route path="/videos" element={<VideosPage />} />
+      <Route path="/exams" element={<ExamsPage />} />
+      <Route path="/affiliate" element={<AffiliatePage />} />
+      <Route path="/afiliado" element={<AffiliateLanding />} />
+    </Routes>
   );
 };
 
