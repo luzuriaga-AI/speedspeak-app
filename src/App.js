d9c457a56b97fdc900aebc8b8c350da6d33c41b9
UNAI LUZURIAGA <unailuzuriaga@MBP-de-UNAI.station>
2025-04-15 02:39:48 +0200

    Añadida ruta /afiliado para AffiliateLanding

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import AuthPage from './pages/AuthPage';
 import BuyCourse from './pages/BuyCourse';
@@ -14,19 +14,21 @@ import AffiliateLanding from './pages/AffiliateLanding'; // ✅ Importar Affilia
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/auth" element={<AuthPage />} />
-      <Route path="/buy" element={<BuyCourse />} />
-      <Route path="/dashboard" element={<UserDashboard />} />
-      <Route path="/checkout" element={<Checkout />} />
-      <Route path="/lesson/:id" element={<Lesson />} />
-      <Route path="/achievements" element={<AchievementsPage />} />
-      <Route path="/videos" element={<VideosPage />} />
-      <Route path="/exams" element={<ExamsPage />} />
-      <Route path="/affiliate" element={<AffiliatePage />} />
-      <Route path="/afiliado" element={<AffiliateLanding />} />
-    </Routes>
+    <Router> {/* ✅ Necesario para que funcionen bien las rutas */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/buy" element={<BuyCourse />} />
+        <Route path="/dashboard" element={<UserDashboard />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/lesson/:id" element={<Lesson />} />
+        <Route path="/achievements" element={<AchievementsPage />} />
+        <Route path="/videos" element={<VideosPage />} />
+        <Route path="/exams" element={<ExamsPage />} />
+        <Route path="/affiliate" element={<AffiliatePage />} />
+        <Route path="/afiliado" element={<AffiliateLanding />} /> {/* ✅ Aquí está la ruta del link */}
+      </Routes>
+    </Router>
   );
 };
 
